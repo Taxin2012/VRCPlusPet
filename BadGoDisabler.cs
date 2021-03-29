c86e6fb343413d04569bbf6ec0ffbad6ff47b3a4
Taxin2012 <26491089+Taxin2012@users.noreply.github.com>
2021-03-29 12:51:19 +0300

    Update BadGoDisabler.cs

--- BadGoDisabler.cs
+++ BadGoDisabler.cs
@@ -1,31 +1,13 @@
 
 using System;
 using UnityEngine;
-using UnhollowerRuntimeLib;
-using System.Runtime.InteropServices;
 
 namespace VRCPlusPet
 {
     internal class BadGoDisabler : MonoBehaviour
     {
-        public Delegate ReferencedDelegate;
-        public IntPtr MethodInfo;
         public BadGoDisabler(IntPtr obj0) : base(obj0)
         {
-            ClassInjector.DerivedConstructorBody(this);
-        }
-        public BadGoDisabler(Delegate referencedDelegate, IntPtr methodInfo) : base(ClassInjector.DerivedConstructorPointer<BadGoDisabler>())
-        {
-            ClassInjector.DerivedConstructorBody(this);
-
-            ReferencedDelegate = referencedDelegate;
-            MethodInfo = methodInfo;
-        }
-        ~BadGoDisabler()
-        {
-            Marshal.FreeHGlobal(MethodInfo);
-            MethodInfo = IntPtr.Zero;
-            ReferencedDelegate = null;
         }
 
         void OnEnable() => this.gameObject.SetActive(false);
